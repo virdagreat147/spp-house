4b6bb00718e1dcb0fcef1b0dc317f36d19afcc10
coderboi4055 <virarora41@gmail.com>
2022-04-04 16:01:25 -0400

    Updates

---
+++ _next/static/R9T-Am9wxtVKIvXs94Rps/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-91b673e83771409c330d.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
